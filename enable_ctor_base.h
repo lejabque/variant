33dd86861cfc8fc7387f849e235935367b471535
lejabque <vorkdenis1@gmail.com>
2021-01-13 22:11:17 +0300

    Remove wrong constructor bases

--- enable_ctor_base.h
+++ enable_ctor_base.h
@@ -13,32 +13,6 @@ struct enable_default_ctor<false> {
   constexpr enable_default_ctor& operator=(enable_default_ctor&&) noexcept = default;
 };
 
-template<bool has_copy_ctor = true>
-struct enable_copy_ctor {};
-
-template<>
-struct enable_copy_ctor<false> {
-  constexpr enable_copy_ctor() noexcept = default;
-  constexpr enable_copy_ctor(enable_copy_ctor const&) noexcept = delete;
-  constexpr enable_copy_ctor(enable_copy_ctor&&) noexcept = default;
-
-  constexpr enable_copy_ctor& operator=(enable_copy_ctor const&) noexcept = default;
-  constexpr enable_copy_ctor& operator=(enable_copy_ctor&&) noexcept = default;
-};
-
-template<bool has_move_ctor = true>
-struct enable_move_ctor {};
-
-template<>
-struct enable_move_ctor<false> {
-  constexpr enable_move_ctor() noexcept = default;
-  constexpr enable_move_ctor(enable_move_ctor const&) noexcept = default;
-  constexpr enable_move_ctor(enable_move_ctor&&) noexcept = delete;
-
-  constexpr enable_move_ctor& operator=(enable_move_ctor const&) noexcept = default;
-  constexpr enable_move_ctor& operator=(enable_move_ctor&&) noexcept = default;
-};
-
 template<bool has_copy_assign = true>
 struct enable_copy_assign {};
 
--- variant_traits.h
+++ variant_traits.h
@@ -6,7 +6,7 @@ namespace variant_utils {
 template<typename... Ts>
 struct variant_traits {
   struct noexcept_value {
-    static constexpr bool default_ctor = (std::is_nothrow_default_constructible_v<types_at_t<0, Ts...>>);
+    static constexpr bool default_ctor = (std::is_nothrow_default_constructible_v<types_at_t < 0, Ts...>>);
     static constexpr bool move_assign = ((std::is_nothrow_move_constructible_v<Ts>
         && std::is_nothrow_move_assignable_v<Ts>) && ...);
     static constexpr bool move_ctor = (std::is_nothrow_move_constructible_v<Ts> && ...);
@@ -16,22 +16,22 @@ struct variant_traits {
   struct trivial {
     static constexpr bool copy_assign = ((std::is_trivially_copy_constructible_v<Ts>
         && std::is_trivially_copy_assignable_v<Ts> && std::is_trivially_destructible_v<Ts>)&& ...);
-    static constexpr bool copy_ctor = (std::is_trivially_copy_constructible_v<Ts>&& ...);
+    static constexpr bool
+        copy_ctor = (std::is_trivially_copy_constructible_v<Ts>&& ...) || (!std::is_copy_constructible_v<Ts>|| ...);
     static constexpr bool move_assign = ((std::is_trivially_move_constructible_v<Ts>
         && std::is_trivially_move_assignable_v<Ts> && std::is_trivially_destructible_v<Ts>) && ...);
-    static constexpr bool move_ctor = (std::is_trivially_move_constructible_v<Ts>&& ...);
+    static constexpr bool
+        move_ctor = (std::is_trivially_move_constructible_v<Ts>&& ...) || (!std::is_move_constructible_v<Ts>|| ...);
     static constexpr bool dtor = (std::is_trivially_destructible_v<Ts>&& ...);
   };
 };
 
 template<typename... Ts>
 struct enable_bases
-    : enable_default_ctor<std::is_default_constructible_v<types_at_t<0, Ts...>>>,
-      enable_copy_ctor<(std::is_copy_constructible_v<Ts> && ...)>,
-      enable_move_ctor<(std::is_move_constructible_v<Ts> && ...)>,
-      enable_copy_assign<((std::is_copy_constructible_v<Ts>
-          && std::is_copy_assignable_v<Ts>) && ...)>,
-      enable_move_assign<((std::is_move_constructible_v<Ts>
-          && std::is_move_assignable_v<Ts>) && ...)> {
+    : enable_default_ctor<std::is_default_constructible_v<types_at_t < 0, Ts...>>>,
+enable_copy_assign<((std::is_copy_constructible_v<Ts>
+    && std::is_copy_assignable_v<Ts>) && ...)>,
+enable_move_assign<((std::is_move_constructible_v<Ts>
+    && std::is_move_assignable_v<Ts>) && ...)> {
 };
 } // namespace variant_utils
